84543e53824b843be6cae9e9b5c190ad1f74c15d
Andre Nonaka <42698192+Andre-Nonaka@users.noreply.github.com>
2022-06-08 22:42:48 -0400

    Update main.js

--- script/main.js
+++ script/main.js
@@ -9,24 +9,36 @@ var expertModal3 = document.getElementById("expert3");
 var expertModal4 = document.getElementById("expert4");
 var expertModal5 = document.getElementById("expert5");
 var expertModal6 = document.getElementById("expert6");
-  
-expertModal1.onclick = function openLink() {
-    window.location.href = 'expert1.html';
+
+if(expertModal1) {
+    expertModal1.onclick = function openLink() {
+        window.location.href = 'expert1.html';
+    }
 }
-expertModal2.onclick = function openLink2() {
-    window.location.href = 'expert2.html';
+if(expertModal2) {
+    expertModal2.onclick = function openLink2() {
+        window.location.href = 'expert2.html';
+    }
 }
-expertModal3.onclick = function openLink3() {
-    window.location.href = 'expert3.html';
+if(expertModal3) {
+    expertModal3.onclick = function openLink3() {
+        window.location.href = 'expert3.html';
+    }
 }
-expertModal4.onclick = function openLink4() {
-    window.location.href = 'expert4.html';
+if(expertModal4) {
+    expertModal4.onclick = function openLink4() {
+        window.location.href = 'expert4.html';
+    }
 }
-expertModal5.onclick = function openLink5() {
-    window.location.href = 'expert5.html';
+if(expertModal5) {
+    expertModal5.onclick = function openLink5() {
+        window.location.href = 'expert5.html';
+    }
 }
-expertModal6.onclick = function openLink6() {
-    window.location.href = 'expert6.html';
+if(expertModal6) {
+    expertModal6.onclick = function openLink6() {
+        window.location.href = 'expert6.html';
+    }
 }
 
 var boxModal1 = document.getElementById("box1");
@@ -35,24 +47,36 @@ var boxModal3 = document.getElementById("box3");
 var boxModal4 = document.getElementById("box4");
 var boxModal5 = document.getElementById("box5");
 var boxModal6 = document.getElementById("box6");
-  
-boxModal1.onclick = function openLink7() {
-    window.location.href = 'webdev.html';
+
+if(boxModal1) {
+    boxModal1.onclick = function openLink7() {
+        window.location.href = 'webdev.html';
+    }
 }
-boxModal2.onclick = function openLink8() {
-    window.location.href = 'webdesign.html';
+if(boxModal2) {
+    boxModal2.onclick = function openLink8() {
+        window.location.href = 'webdesign.html';
+    }
 }
-boxModal3.onclick = function openLink9() {
-    window.location.href = 'graphicdesign.html';
+if(boxModal3) {
+    boxModal3.onclick = function openLink9() {
+        window.location.href = 'graphicdesign.html';
+    }
 }
-boxModal4.onclick = function openLink10() {
-    window.location.href = 'appdev.html';
+if(boxModal4) {
+    boxModal4.onclick = function openLink10() {
+        window.location.href = 'appdev.html';
+    }
 }
-boxModal5.onclick = function openLink11() {
-    window.location.href = 'SEO.html';
+if(boxModal5) {
+    boxModal5.onclick = function openLink11() {
+        window.location.href = 'SEO.html';
+    }
 }
-boxModal6.onclick = function openLink12() {
-    window.location.href = 'hosting.html';
+if(boxModal6) {
+    boxModal6.onclick = function openLink12() {
+        window.location.href = 'hosting.html';
+    }
 }
 
 var nav1 = document.getElementById("homeNav");
@@ -62,51 +86,63 @@ var nav4 = document.getElementById("expertsNav");
 var nav5 = document.getElementById("officeNav");
 var nav6 = document.getElementById("contactNav");
 
-nav1.onclick = function clickNav1() {
-    nav1.classList.add("active");
-    nav2.classList.remove("active");
-    nav3.classList.remove("active");
-    nav4.classList.remove("active");
-    nav5.classList.remove("active");
-    nav6.classList.remove("active");
+if(nav1) {
+    nav1.onclick = function clickNav1() {
+        nav1.classList.add("active");
+        nav2.classList.remove("active");
+        nav3.classList.remove("active");
+        nav4.classList.remove("active");
+        nav5.classList.remove("active");
+        nav6.classList.remove("active");
+    }
 }
-nav2.onclick = function clickNav2() {
-    nav2.classList.add("active");
-    nav1.classList.remove("active");
-    nav3.classList.remove("active");
-    nav4.classList.remove("active");
-    nav5.classList.remove("active");
-    nav6.classList.remove("active");
+if(nav2) {
+    nav2.onclick = function clickNav2() {
+        nav2.classList.add("active");
+        nav1.classList.remove("active");
+        nav3.classList.remove("active");
+        nav4.classList.remove("active");
+        nav5.classList.remove("active");
+        nav6.classList.remove("active");
+    }
 }
-nav3.onclick = function clickNav3() {
-    nav3.classList.add("active");
-    nav2.classList.remove("active");
-    nav1.classList.remove("active");
-    nav4.classList.remove("active");
-    nav5.classList.remove("active");
-    nav6.classList.remove("active");
+if(nav3) {
+    nav3.onclick = function clickNav3() {
+        nav3.classList.add("active");
+        nav2.classList.remove("active");
+        nav1.classList.remove("active");
+        nav4.classList.remove("active");
+        nav5.classList.remove("active");
+        nav6.classList.remove("active");
+    }
 }
-nav4.onclick = function clickNav4() {
-    nav4.classList.add("active");
-    nav2.classList.remove("active");
-    nav3.classList.remove("active");
-    nav1.classList.remove("active");
-    nav5.classList.remove("active");
-    nav6.classList.remove("active");
+if(nav4) {
+    nav4.onclick = function clickNav4() {
+        nav4.classList.add("active");
+        nav2.classList.remove("active");
+        nav3.classList.remove("active");
+        nav1.classList.remove("active");
+        nav5.classList.remove("active");
+        nav6.classList.remove("active");
+    }
 }
-nav5.onclick = function clickNav5() {
-    nav5.classList.add("active");
-    nav2.classList.remove("active");
-    nav3.classList.remove("active");
-    nav4.classList.remove("active");
-    nav1.classList.remove("active");
-    nav6.classList.remove("active");
+if(nav5) {
+    nav5.onclick = function clickNav5() {
+        nav5.classList.add("active");
+        nav2.classList.remove("active");
+        nav3.classList.remove("active");
+        nav4.classList.remove("active");
+        nav1.classList.remove("active");
+        nav6.classList.remove("active");
+    }
 }
-nav6.onclick = function clickNav6() {
-    nav6.classList.add("active");
-    nav2.classList.remove("active");
-    nav3.classList.remove("active");
-    nav4.classList.remove("active");
-    nav5.classList.remove("active");
-    nav1.classList.remove("active");
+if(nav6) {
+    nav6.onclick = function clickNav6() {
+        nav6.classList.add("active");
+        nav2.classList.remove("active");
+        nav3.classList.remove("active");
+        nav4.classList.remove("active");
+        nav5.classList.remove("active");
+        nav1.classList.remove("active");
+    }
 }
